5508277a84f17d7807d4eb2f6f4fa9acafc4de55
roxvdp <roxanne.vandputten@gmail.com>
2025-04-28 11:39:13 +0200

    Fixed requirements and host

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -39,4 +40,5 @@ def get_cars():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
